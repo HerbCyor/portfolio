68a2e415e07c4e66d23c4d48935f985b6272ac3f
Herbert <herbert.cardozo@gmail.com>
2021-11-24 17:48:21 -0300

    removing previous *pyc files-addind export_csv to admin panel

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,19 +1,48 @@
 from django.contrib import admin
 from .models import Skill, UserProfile, ContactProfile, Media
+import csv
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+
 # Register your models here.
+
+
+@admin.action(description="Export as csv")
+def export_as_csv(modeladmin, request, queryset):
+    if not request.user.is_staff:
+        raise PermissionDenied
+    opts = queryset.model._meta
+    model = queryset.model
+    response = HttpResponse(content_type="text/csv")
+    # force download.
+    response["Content-Disposition"] = "attachment;filename=export.csv"
+    # the csv writer
+    writer = csv.writer(response)
+    field_names = [field.name for field in opts.fields]
+    # Write a first row with header information
+    writer.writerow(field_names)
+    # Write data rows
+    for obj in queryset:
+        writer.writerow([getattr(obj, field) for field in field_names])
+    return response
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user')
+    list_display = ("id", "user")
+
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'is_active')
+    list_display = ("id", "name", "is_active")
+    actions = [export_as_csv]
+
 
 @admin.register(Media)
 class MediaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ("id", "name")
+
 
 @admin.register(ContactProfile)
 class ContactProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'timestamp', 'name')
+    list_display = ("id", "timestamp", "name")
